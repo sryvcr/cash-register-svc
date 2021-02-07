230054e19f5be693fbd4ce3e487cf08a7735ed38
sryvcr <andresgx9@gmail.com>
2021-02-06 23:23:43 -0500

    create transaction when deposit money

--- src/domain/app_rules/money_inventory/index.ts
+++ src/domain/app_rules/money_inventory/index.ts
@@ -3,10 +3,15 @@ import { buildDepositMoney } from "./use_cases/deposit_money";
 import { buildVacateMoney } from "./use_cases/vacate_money";
 import Logger from "../../../infreastructure/external_interfaces/logger";
 import { MoneyInventoryRepository } from "../../../interface_adapters/repositories/money_inventory/money_inventory_repo";
+import { TransactionsRepository } from "../../../interface_adapters/repositories/transactions/transactions_repo";
 import { MoneyInventoryPsqlRepository } from "../../../infreastructure/storage/postgres/repositories/money_inventory/money_inventory_pgsql_repo";
+import { TransactionsPsqlRepository } from "../../../infreastructure/storage/postgres/repositories/transactions/transactions_pgsql_repo";
 
 const moneyInventoryRepo = new MoneyInventoryRepository(
     new MoneyInventoryPsqlRepository()
+);
+const transactionsRepo = new TransactionsRepository(
+    new TransactionsPsqlRepository()
 );
 
 const getStatus = buildGetStatus({
@@ -14,6 +19,7 @@ const getStatus = buildGetStatus({
 });
 const depositMoney = buildDepositMoney({
     moneyInventoryRepo,
+    transactionsRepo,
     Logger
 });
 const vacateMoney = buildVacateMoney({
--- src/domain/app_rules/money_inventory/use_cases/deposit_money.ts
+++ src/domain/app_rules/money_inventory/use_cases/deposit_money.ts
@@ -1,19 +1,26 @@
 import { coinValidator } from "../../../enterprise_rules/validators/coin_validator";
+import { TransactionsEnum } from "../../../enterprise_rules/dtos/enums/transactions_enum";
+import { IMoneyInventory } from "../../../enterprise_rules/entities/money_inventory/money_inventory_dom";
+import { ITransactions } from "../../../enterprise_rules/entities/transactions/transactions_dom";
 import { makeMoneyInventory } from "../../../enterprise_rules/entities/money_inventory/index";
+import { makeTransaction } from "../../../enterprise_rules/entities/transactions/index";
 
 
 export function buildDepositMoney({
     moneyInventoryRepo,
+    transactionsRepo,
     Logger,
 }: any) {
     const logger = Logger(__filename);
 
-    async function execute(money: any): Promise<any[]> {
+    async function execute(money: any): Promise<any> {
         let t = await moneyInventoryRepo.getTransaction({
             autocommit: false,
         });
         try {
             const result: any = {};
+            const validCoins: any = {};
+            let transactionAmount = 0;
             for (let coin in money) {
                 const coinInt = parseInt(coin);
                 if (coinValidator(coinInt)) {
@@ -21,28 +28,47 @@ export function buildDepositMoney({
                     logger.debug(`coin: ${coin}: ${quantity}`);
                     const totalAmount = coinInt * parseInt(quantity);
                     logger.debug(`total: ${totalAmount}`);
-                    const moneyInventory = await moneyInventoryRepo.getOne({ coin });
+                    const moneyInventory: IMoneyInventory = await moneyInventoryRepo.getOne({ coin });
                     logger.debug(`moneyInventory: ${JSON.stringify(moneyInventory)}`);
                     moneyInventory.quantity += quantity;
                     moneyInventory.total += totalAmount;
                     const entity = makeMoneyInventory(moneyInventory);
-                    const moneyInventoryUpdated = await moneyInventoryRepo.updateOne(
+                    const moneyInventoryUpdated: IMoneyInventory = await moneyInventoryRepo.updateOne(
                         moneyInventory.id,
                         entity,
                         { transaction: t }
                     );
                     logger.debug(`moneyInventoryUpdated: ${JSON.stringify(moneyInventoryUpdated)}`);
                     result[coin] = 'ok';
+                    transactionAmount += totalAmount;
+                    validCoins[coin] = quantity;
                 } else {
                     logger.info(`coin: ${coinInt} not valid`);
                     result[coin] = 'not valid';
                 }
             }
+            await createTransaction(transactionAmount, validCoins, t);
             t.commit();
             return result;
         } catch (error) {
             await t.rollback();
-            throw error
+            throw error;
+        }
+    }
+
+    async function createTransaction(amount: number, coins: any, t: any): Promise<void> {
+        try {
+            const transaction = {
+                id: undefined,
+                amount: amount,
+                type: TransactionsEnum.DEPOSIT,
+                coins: coins,
+            }
+            const entity: ITransactions = makeTransaction(transaction);
+            await transactionsRepo.createOne(entity, { transaction: t });
+            return;
+        } catch (error) {
+            throw error;
         }
     }
 
